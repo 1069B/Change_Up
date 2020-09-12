fcc197fffe445497653442a327d9d54328dad128
RocketMan112 <ckschanker@gmail.com>
2020-09-11 21:03:43 -0600

    Controller Class Version 1

--- include/robot/devices/controller_class.hpp
+++ include/robot/devices/controller_class.hpp
@@ -6,30 +6,37 @@
 namespace CONTROLLER{
   class Button{
   private:
-    pros::controller_id_e_t m_controller_type;// Master or Partner
-    pros::controller_digital_e_t m_button_type;// Which Button
+    pros::controller_id_e_t m_controller_type{};// Master or Partner
+    pros::controller_digital_e_t m_button_type{};// Which Button
 
     bool m_state{};
+    bool m_previous_state{};
 
-    std::function<void()> m_callback_function{};
-    int m_callback_delay_time{};
+    std::function<void()> m_callback_pressed{};
+    std::function<void()> m_callback_released{};
   public:
     /*  Constuctors  */
-    Button();
+    Button(const pros::controller_id_e_t p_controller_type, const pros::controller_digital_e_t p_button_type);
 
     /*  Getting Function  */
     bool get_state(){ return m_state; }
 
-    std::function<void()> get_callback_function(){ return m_callback_function; }
+    bool get_previous_state(){ return m_previous_state; }
 
-    int get_callback_delay_time(){ return m_callback_delay_time; }
+    std::function<void()> get_callback_pressed(){ return m_callback_pressed; }
+
+    std::function<void()> get_callback_released(){ return m_callback_released; }
 
     /*  Setter Function  */
-    void set_callback_delay_time(int p_callback_delay_time){ m_callback_delay_time = p_callback_delay_time; }
+    void set_previous_state(const bool p_previous_state){ m_previous_state = p_previous_state; }
 
-    void set_callback_function(std::function<void()> p_callback_function){ m_callback_function = p_callback_function; }
+    void set_callback_pressed(std::function<void()> p_callback_pressed){ m_callback_pressed = p_callback_pressed; }
+
+    void set_callback_released(std::function<void()> p_callback_released){ m_callback_released = p_callback_released; }
 
     /*  Action Functions */
+    void check_state();
+
     bool check_callback();
   };
 
@@ -41,45 +48,46 @@ namespace CONTROLLER{
     short m_percent{};
   public:
     /*  Constuctors  */
-    Joystick();
+    Joystick(pros::controller_id_e_t p_controller_type, pros::controller_analog_e_t p_axis_type);
     /*  Getting Function  */
     short get_percent(){ return m_percent; }
 
     /*  Setter Function  */
     void set_percent(short p_percent){ m_percent = p_percent; }
+
+    /*  Action Function */
+    void check_percent();
+  };
+
+  class Controller{
+  private:
+
+  public:
+    /*  Constuctors  */
+    Controller();
+    /*  Getting Function  */
+
+    /*  Setter Function  */
+
+    /*  Action Functions */
+    Button ButtonL1;
+    Button ButtonL2;
+    Button ButtonR1;
+    Button ButtonR2;
+    Button ButtonUp;
+    Button ButtonDown;
+    Button ButtonLeft;
+    Button ButtonRight;
+    Button ButtonX;
+    Button ButtonB;
+    Button ButtonY;
+    Button ButtonA;
+    Joystick Axis1;
+    Joystick Axis2;
+    Joystick Axis3;
+    Joystick Axis4;
   };
 
 }
-
-class Controller{
-private:
-
-public:
-  /*  Constuctors  */
-  Controller();
-  /*  Getting Function  */
-
-  /*  Setter Function  */
-
-  /*  Action Functions */
-  CONTROLLER::Button ButtonL1;
-  CONTROLLER::Button ButtonL2;
-  CONTROLLER::Button ButtonR1;
-  CONTROLLER::Button ButtonR2;
-  CONTROLLER::Button ButtonUp;
-  CONTROLLER::Button ButtonDown;
-  CONTROLLER::Button ButtonLeft;
-  CONTROLLER::Button ButtonRight;
-  CONTROLLER::Button ButtonX;
-  CONTROLLER::Button ButtonB;
-  CONTROLLER::Button ButtonY;
-  CONTROLLER::Button ButtonA;
-  CONTROLLER::Joystick Axis1;
-  CONTROLLER::Joystick Axis2;
-  CONTROLLER::Joystick Axis3;
-  CONTROLLER::Joystick Axis4;
-};
-
-
 
 #endif // CONTROLLER_CLASS_H
--- src/robot/devices/controller_class.cpp
+++ src/robot/devices/controller_class.cpp
@@ -1,15 +1,37 @@
 #include "robot/devices/controller_class.hpp"
 
-using namespace CONTROLLER;
-
-Button::Button(){
-
+/* Button Class*/
+CONTROLLER::Button::Button(const pros::controller_id_e_t p_controller_type, const pros::controller_digital_e_t p_button_type){
+  m_controller_type = p_controller_type;
+  m_button_type = p_button_type;
 }
 
-bool Button::check_callback(){
-  if(get_state()){ //TODO: ADD Callback delay once Timer is done.
-    m_callback_function();
-    return true;
+void CONTROLLER::Button::check_state(){
+  m_state = pros::c::controller_get_digital(m_controller_type, m_button_type);
+}
+
+bool CONTROLLER::Button::check_callback(){
+  if(get_state() && get_state() != get_previous_state()){
+    set_previous_state(get_state());
+    m_callback_pressed();
+    return 1;
   }
-  return false;
+  else if(!get_state() && get_state() != get_previous_state()){
+    set_previous_state(get_state());
+    m_callback_released();
+    return -1;
+  }
+  return 0;
+}
+
+/* Joystick Class */
+CONTROLLER::Joystick::Joystick(const pros::controller_id_e_t p_controller_type, const pros::controller_analog_e_t p_axis_type){
+  m_controller_type = p_controller_type;
+  m_axis_type = p_axis_type;
+}
+
+void CONTROLLER::Joystick::check_percent(){
+  short l_joystick_raw_value = pros::c::controller_get_analog(m_controller_type, m_axis_type);
+  m_percent = (short)((l_joystick_raw_value/127.0)*100.0);
+
 }
