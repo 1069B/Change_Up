cc88e1c1178d6fb8950f44dc6ed4a5418d08bba6
RocketMan112 <ckschanker@gmail.com>
2020-10-17 11:11:47 -0600

    Initial Data Storing Class V1.0

--- include/robot/devices/data_storage_class.hpp
+++ include/robot/devices/data_storage_class.hpp
@@ -1,27 +1,51 @@
 #include "main.h"
+#include "pugi/pugiconfig.hpp"
+#include "pugi/pugixml.hpp"
 
 #ifndef DATALOGGING_H
 #define DATALOGGING_H
 
 class Data_Storing{
 private:
-  pugi::xml_documentm_doc{};
   std::string m_address{};
+  pugi::xml_document m_doc;
 
+  std::string m_class_name{};
+  std::string m_instance_name{};
+
+  /* Internal Functions */
+  void check_node(pugi::xml_node p_node, std::string const p_node_name);
+
+  pugi::xml_attribute retreive_attribute(std::string const p_varible_name, std::string const p_varible_type);
 public:
   /*  Constructors  */
-  Data_Storing(std::string const p_address);
+  Data_Storing(std::string const p_address, std::string const p_class_name, std::string const p_instance_name);
 
-  /*  Getter  */
-  std::fstream& get_file(){ return m_file; };
-
+  /*  Getter Functions  */
   std::string get_address(){ return m_address; };
 
-  static bool get_sd_card_connected(){ return pros::c::usd_is_installed(); };
+  std::string get_class_name(){ return m_class_name; };
 
-  /*  Setter  */
+  std::string get_instance_name(){ return m_instance_name; };
 
-  /*  Action  */
+  /*  File Write Functions  */
+  void store_int(std::string const p_varible_name, int const p_value);
+
+  void store_double(std::string const p_varible_name, double const p_value);
+
+  void store_bool(std::string const p_varible_name, bool const p_value);
+
+  void store_string(std::string const p_varible_name, std::string const p_value);
+
+  /*  File Read Fucntions  */
+  int read_int(std::string const p_varible_name);
+
+  double read_double(std::string const p_varible_name);
+
+  bool read_bool(std::string const p_varible_name);
+
+  std::string read_string(std::string const p_varible_name);
+
 };
 
 #endif // DATALOGGING_H
---
+++ src/robot/devices/data_storage_class.cpp
@@ -0,0 +1,63 @@
+#include "robot/devices/data_storage_class.hpp"
+
+void Data_Storing::check_node(pugi::xml_node p_node, std::string const p_node_name){
+    if(!p_node.child(p_node_name.c_str())){// Node does not exist
+        p_node.append_child(p_node_name.c_str());
+    }
+}
+
+pugi::xml_attribute Data_Storing::retreive_attribute(std::string const p_varible_name, std::string const p_varible_type){
+    m_doc.load_file(m_address.c_str());
+    pugi::xml_node l_node = m_doc.child(m_class_name.c_str()).child(m_instance_name.c_str());
+
+    if(!l_node.child(p_varible_name.c_str())){
+        pugi::xml_node temp = l_node.append_child(p_varible_name.c_str());
+        temp.append_attribute(p_varible_type.c_str());
+        return temp.attribute(p_varible_type.c_str());
+    }
+    else{
+        pugi::xml_node temp = l_node.child(p_varible_name.c_str());
+        return temp.attribute(p_varible_type.c_str());
+    }
+}
+
+Data_Storing::Data_Storing(std::string const p_address, std::string const p_class_name, std::string const p_instance_name){
+    m_address = p_address;
+    m_class_name = p_class_name;
+    m_instance_name = p_instance_name;
+
+    m_doc.load_file(m_address.c_str());
+    check_node(m_doc, m_class_name);// Checks to make sure that the class node exists
+    check_node(m_doc.child(m_class_name.c_str()), m_instance_name.c_str());// Checks to make sure that the instance node exists
+    m_doc.save_file(m_address.c_str());
+};
+
+void Data_Storing::store_int(std::string const p_varible_name, int const p_value){
+    retreive_attribute(p_varible_name, "int").set_value(p_value);
+    m_doc.save_file(m_address.c_str());
+}
+void Data_Storing::store_double(std::string const p_varible_name, double const p_value){
+    retreive_attribute(p_varible_name, "double").set_value(p_value);
+    m_doc.save_file(m_address.c_str());
+}
+void Data_Storing::store_bool(std::string const p_varible_name, bool const p_value){
+    retreive_attribute(p_varible_name, "bool").set_value(p_value);
+    m_doc.save_file(m_address.c_str());
+}
+void Data_Storing::store_string(std::string const p_varible_name, std::string const p_value){
+    retreive_attribute(p_varible_name, "string").set_value(p_value.c_str());
+    m_doc.save_file(m_address.c_str());
+}
+
+int Data_Storing::read_int(std::string const p_varible_name){
+    return retreive_attribute(p_varible_name, "int").as_int();
+}
+double Data_Storing::read_double(std::string const p_varible_name){
+    return retreive_attribute(p_varible_name, "double").as_double();
+}
+bool Data_Storing::read_bool(std::string const p_varible_name){
+    return retreive_attribute(p_varible_name, "bool").as_bool();
+}
+std::string Data_Storing::read_string(std::string const p_varible_name){
+    return retreive_attribute(p_varible_name, "string").as_string();
+}
