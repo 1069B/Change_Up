4574cc823b682efec8eadc2875186448db32ef3a
RocketMan112 <ckschanker@gmail.com>
2020-11-07 19:26:00 -0700

    Intial Version V1.0 of Switch Class

--- include/robot/graphical_interface/switch_class.hpp
+++ include/robot/graphical_interface/switch_class.hpp
@@ -18,12 +18,22 @@ private:
   lv_style_t* m_false_style{};
 
   bool m_state{false};
+
+  /*Required Variles for a Connected Bool*/
+  bool m_connected_bool = false;
+  bool* m_connected_bool_pointer;
+
+  /*Required Variles for a Connected Int*/
+  bool m_connected_int = false;
+  int* m_connected_int_pointer;
+  int m_connected_int_value_true;
+  int m_connected_int_value_false;
 public:
   /*  Constuctors  */
   Switch(short const p_xOrgin, short const p_yOrgin, int const p_state, lv_style_t& p_background_style,
     lv_style_t& p_indicator_style, lv_style_t& p_true_style, lv_style_t& p_false_style);
 
-  /*  Getting Function  */
+  /*  Getter Function  */
   short get_xOrgin(){ return m_xOrigin; }
 
   short get_yOrigin(){ return m_yOrigin; }
@@ -51,12 +61,16 @@ public:
 
   void set_switch_false_style(lv_style_t& p_false_style){ m_false_style = &p_false_style; }
 
-  void set_state(bool const p_state){ m_state = p_state; }
+  void set_state(bool const p_state);
 
   /*  Action Function */
   void draw_switch();// TODO: Add Screen& p_screen
 
   void update_switch();// TODO: Add Screen& p_screen
+
+  void add_connected_bool(bool& p_connected_bool_pointer);
+
+  void add_connected_int(int& p_connected_int_pointer, int const p_true_value, int const p_false_value);
 };
 
 #endif // RECTAGNLE_CLASS_H
--- src/robot/graphical_interface/switch_class.cpp
+++ src/robot/graphical_interface/switch_class.cpp
@@ -10,16 +10,26 @@ Switch::Switch(short const p_xOrgin, short const p_yOrgin, int const p_state, lv
   m_background_style = &p_background_style;
   m_indicator_style = &p_indicator_style;
   m_true_style = &p_true_style;
-  m_false_style = &p_false_style
+  m_false_style = &p_false_style;
+}
+
+/* Setter Functions*/
+void Switch::set_state(bool const p_state){
+  m_state = p_state;
+  if(m_state)
+    lv_sw_on(m_switch);
+  else
+    lv_sw_off(m_switch);
+  update_switch();
 }
 
 /*  Action Function */
 void Switch::draw_switch(){
   m_switch = lv_sw_create(lv_scr_act(), NULL); // TODO: Change parent
-  lv_sw_set_style(m_switch, LV_SW_STYLE_BG, &bg_style);
-  lv_sw_set_style(m_switch, LV_SW_STYLE_INDIC, &indic_style);
-  lv_sw_set_style(m_switch, LV_SW_STYLE_KNOB_ON, &knob_on_style);
-  lv_sw_set_style(m_switch, LV_SW_STYLE_KNOB_OFF, &knob_off_style);
+  lv_sw_set_style(m_switch, LV_SW_STYLE_BG, m_background_style);
+  lv_sw_set_style(m_switch, LV_SW_STYLE_INDIC, m_indicator_style);
+  lv_sw_set_style(m_switch, LV_SW_STYLE_KNOB_ON, m_true_style);
+  lv_sw_set_style(m_switch, LV_SW_STYLE_KNOB_OFF, m_false_style);
   lv_obj_align(m_switch, lv_scr_act(), LV_ALIGN_IN_TOP_LEFT, m_xOrigin, m_yOrigin); // TODO: Change parent
 
   if(m_state)
@@ -27,13 +37,35 @@ void Switch::draw_switch(){
   else
     lv_sw_off(m_switch);
 
+  update_switch();
 }
 
 void Switch::update_switch(){
-  if(lv_sw_get_state()){
-    m_state = true
+  m_state = lv_sw_get_state(m_switch);
+
+  if(m_state){
+    if(m_connected_bool)
+      *m_connected_bool_pointer = true;
+    if(m_connected_int_pointer)
+      *m_connected_int_pointer = m_connected_int_value_true;
   }
-  else(){
-    m_state = false;
+
+  else{
+    if(m_connected_bool)
+      *m_connected_bool_pointer = false;
+    if(m_connected_int_pointer)
+      *m_connected_int_pointer = m_connected_int_value_false;
   }
+}
+
+void Switch::add_connected_bool(bool& p_connected_bool_pointer){
+  m_connected_bool_pointer = &p_connected_bool_pointer;
+  m_connected_bool = true;
+}
+
+void Switch::add_connected_int(int& p_connected_int_pointer, int const p_true_value, int const p_false_value){
+  m_connected_int_pointer = &p_connected_int_pointer;
+  m_connected_int_value_true = p_true_value;
+  m_connected_int_value_false = p_false_value;
+  m_connected_int = true;
 }
