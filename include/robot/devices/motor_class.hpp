4b3ee44eb12b9ef075f0bca4a4145b4360a1bf99
RocketMan112 <ckschanker@gmail.com>
2020-11-23 20:34:49 -0700

    Initial Motor Class V1.0

--- include/robot/devices/motor_class.hpp
+++ include/robot/devices/motor_class.hpp
@@ -4,6 +4,11 @@
 #ifndef MOTOR_CLASS_H
 #define MOTOR_CLASS_H
 
+enum Motor_Mode{
+  MOTOR_VELOCITY_DEPENDENT = 0,
+  MOTOR_VOLTAGE_DEPENDENT = 1
+};
+
 class Motor{
 private:
   Robot &m_robot;
@@ -15,29 +20,74 @@ private:
   pros::motor_brake_mode_e_t m_brake_mode{};
   pros::motor_gearset_e_t m_motor_gearset{};
 
-  Data_Storing m_settings{"Settings.xml", "Motors", m_name};
+  Data_Storing m_settings{"Settings.xml", "Motors", m_name };
 
+  Motor_Mode m_mode{MOTOR_VELOCITY_DEPENDENT};
   int m_desired_velocity;
-
   int m_desired_voltage;
 
-  int m_velocity;
-
-  int m_zero_position;
-
-  int m_tempature;
-
-  int m_torque;
 public:
   /*  Constuctors  */
-  Motor(Robot &p_robot, std::string const p_name, short const p_port, pros::motor_gearset_e_t const m_motor_gearset, bool const m_reversed);
-  /*  Getting Function  */
-  std::string get_name(){ return m_name; };
+  Motor(Robot &p_robot, std::string const p_name, short const p_port, pros::motor_gearset_e_t const p_motor_gearset, pros::motor_brake_mode_e_t const p_motor_brake, bool const p_reversed);
+
+  /*  Getting Functions  */
+  std::string get_name(){ return m_name; }
+
+  short get_port(){ return m_port; }
+
+  bool get_reversed(){ return m_reversed; }
+
+  pros::motor_brake_mode_e_t get_brake_mode(){ return m_brake_mode; }
+
+  pros::motor_gearset_e_t get_motor_gearset(){ return m_motor_gearset; }
+
+  int get_actual_velocity(){return pros::c::motor_get_actual_velocity(m_port); }
+
+  int get_actual_voltage(){ return pros::c::motor_get_voltage(m_port); }
+
+  int get_current_draw(){ return pros::c::motor_get_current_draw(m_port); }
+
+  int get_power(){ return pros::c::motor_get_power(m_port); }
+
+  int get_efficiency(){ return pros::c::motor_get_efficiency(m_port); }
+
+  int get_tempature(){ return pros::c::motor_get_temperature(m_port) ;}
+
+  int get_direction(){ return pros::c::motor_get_direction(m_port); }// TODO: Does this return 0 when not moving
+
+  int is_over_current(){ return pros::c::motor_is_over_current(m_port); }
+
+  int is_over_temp(){ return pros::c::motor_is_over_temp(m_port); }
+
+  bool is_stopped(){ return pros::c::motor_is_stopped(m_port); }
+
+  int get_postion(){ return pros::c::motor_get_position(m_port); }
+
+  int get_torque(){ return pros::c::motor_get_torque(m_port); }
+
+  int get_desired_velocity(){ return m_desired_velocity; }
+
+  int get_desired_voltage(){ return m_desired_voltage; }
+
+  bool get_connected(){ return !(get_tempature() == 2147483647); }
   /*  Setter Function  */
+  void set_name(std::string const p_name){ m_name = p_name; }
+
+  void set_port(short p_port);
+
+  void set_reversed(bool p_reversed);
+
+  void set_brake_mode(pros::motor_brake_mode_e_t p_brake_mode);
+
+  void set_motor_gearset(pros::motor_gearset_e_t p_motor_gearset);
+
+  void set_desired_velocity(int p_desired_velocity);
+
+  void set_desired_voltage(int p_voltage);
 
   /*  Action Functions */
+  void reset_position(){ pros::c::motor_tare_position(m_port); }
 
 };
-
 
 #endif // MOTOR_CLASS_H
--- src/robot/devices/motor_class.cpp
+++ src/robot/devices/motor_class.cpp
@@ -1,11 +1,12 @@
 #include "robot/devices/motor_class.hpp"
 #include "robot/robot_class.hpp"
 
-Motor::Motor(Robot &p_robot, std::string const p_name, short const p_port, pros::motor_gearset_e_t const p_motor_gearset, bool const p_reversed):
+Motor::Motor(Robot &p_robot, std::string const p_name, short const p_port, pros::motor_gearset_e_t const p_motor_gearset, pros::motor_brake_mode_e_t const p_motor_brake, bool const p_reversed):
 m_robot(p_robot){
   if(m_robot.get_recall_settings()){
     m_port = m_settings.intitialize_int("Port", p_port);
     m_motor_gearset = (pros::motor_gearset_e_t)m_settings.intitialize_int("Gearset", (int)p_motor_gearset);
+    m_brake_mode = (pros::motor_brake_mode_e_t)m_settings.intitialize_int("Brake", (int)p_motor_brake);
     m_reversed = m_settings.intitialize_bool("Reversed", p_reversed);
   }
   else{
@@ -14,4 +15,47 @@ m_robot(p_robot){
     m_motor_gearset = p_motor_gearset;
     m_reversed = p_reversed;
   }
+
+  pros::c::motor_set_gearing(m_port, m_motor_gearset);
+  pros::c::motor_set_brake_mode(m_port, m_brake_mode);
+  pros::c::motor_set_reversed(m_port, m_reversed);
+  pros::c::motor_set_encoder_units(m_port, pros::E_MOTOR_ENCODER_ROTATIONS);
+
+
+}
+
+/* Setter Function */
+void Motor::set_port(short const p_port){
+  m_port = p_port;
+  m_settings.store_int("Port", m_port);
+}
+
+void Motor::set_reversed(bool const p_reversed){
+  m_reversed = p_reversed;
+  m_settings.store_bool("Reversed", m_reversed);
+}
+
+void Motor::set_brake_mode(pros::motor_brake_mode_e_t const p_brake_mode){
+  m_brake_mode = p_brake_mode;
+  m_settings.store_int("Brake", m_brake_mode);
+}
+
+void Motor::set_motor_gearset(pros::motor_gearset_e_t const p_motor_gearset){
+  m_motor_gearset = p_motor_gearset;
+  m_settings.store_int("Gearset", m_motor_gearset);
+  pros::c::motor_set_gearing(m_port, m_motor_gearset);
+}
+
+void Motor::set_desired_velocity(int const p_desired_velocity){
+  m_desired_velocity = p_desired_velocity;
+  m_mode = MOTOR_VELOCITY_DEPENDENT;
+  m_desired_voltage = 0;
+  pros::c::motor_move_velocity(m_port, m_desired_velocity);
+}
+
+void Motor::set_desired_voltage(int const p_desired_voltage){
+  m_desired_voltage = p_desired_voltage;
+  m_mode = MOTOR_VOLTAGE_DEPENDENT;
+  m_desired_velocity = 0;
+  pros::c::motor_move_voltage(m_port, m_desired_voltage);
 }
